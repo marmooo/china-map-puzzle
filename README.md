6df268ba36cbb6bcb7c9c734d360e6085730f57c
marmooo <marmooo@users.noreply.github.com>
2023-09-02 10:49:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# china-map-puzzle
+
+[A map puzzle of Chinese administrative divisions](https://marmooo.github.io/china-map-puzzle/).
+
+## Build
+
+```
+bash build.sh
+```
+
+## Related Projects
+
+- [Japan Map Puzzle](https://marmooo.github.io/japan-map-puzzle/)
+- [USA Map Puzzle](https://marmooo.github.io/usa-map-puzzle/)
+
+## Attribution
+
+- `map-original.svg` is cloned from
+  [Wikimedia](https://commons.wikimedia.org/wiki/File:China_blank_map.svg)
+  ([SVG](https://upload.wikimedia.org/wikipedia/commons/2/23/China_blank_map_grey.svg))
+  licensed under the CC-BY-SA-4.0
+- `src/map.svg` is modified from `map-original.svg` licensed under the
+  CC-BY-SA-4.0
+- `src/favicon/favicon.svg` is modified from
+  [mapsicon](https://github.com/djaiss/mapsicon)
---
+++ src/en/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "2023-09-01 21:30";
+const urlsToCache = [
+  "/china-map-puzzle/",
+  "/china-map-puzzle/en/",
+  "/china-map-puzzle/index.js",
+  "/china-map-puzzle/map.svg",
+  "/china-map-puzzle/data/en.lst",
+  "/china-map-puzzle/mp3/decision50.mp3",
+  "/china-map-puzzle/mp3/correct1.mp3",
+  "/china-map-puzzle/mp3/correct3.mp3",
+  "/china-map-puzzle/favicon/favicon.svg",
+  "https://cdn.jsdelivr.net/npm/fabric@5.3.0/dist/fabric.min.js",
+  "https://cdn.jsdelivr.net/npm/svgpath@2.6.0/+esm",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
+      );
+    }),
+  );
+});
